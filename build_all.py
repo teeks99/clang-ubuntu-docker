d1b07f0dd604be0c38a910d9835ed67061650b99
Thomas Kent <tom@teeks99.com>
2021-10-02 08:23:33 -0500

    Combined Build

--- build_all.py
+++ build_all.py
@@ -4,8 +4,14 @@ import subprocess
 repo = 'test/clang'
 
 versions = [
-    "2.9", "3.0", "3.1", "3.2", "3.3", "3.4", "3.5", "3.6", "3.7", "3.8", "3.9",
-    "4", "5", "6", "7", "8", "9", "10", "11", "12",
+    # Trusty
+    #"2.9", "3.0", "3.1", "3.2", "3.3", "3.4", "3.5", "3.6", "3.7", "3.8", 
+    # Xenial
+    "3.9", "4", "5", "6", 
+    # Bionic
+    "7", "8", "9", "10", 
+    # Focal
+    "11", "12", "13", "14"
     ]
 
 if len(sys.argv) == 2:
@@ -14,4 +20,8 @@ if len(sys.argv) == 2:
 for version in versions:
     cmd = f"docker build clang-{version} -t {repo}:{version}"
     print(cmd)
-    subprocess.check_call(cmd, shell=True)
+    try:
+        subprocess.check_call(cmd, shell=True)
+    except:
+        print("Failure in command: " + cmd)
+        raise
