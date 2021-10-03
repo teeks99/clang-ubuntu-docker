c8e107eff8168b3912e914375b97106e4af3fe53
Thomas Kent <tom@teeks99.com>
2021-10-03 10:48:02 -0500

    Build, test, and tag moved out to methods

--- build_all.py
+++ build_all.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import datetime
 
 repo = 'test/clang'
 
@@ -14,14 +15,56 @@ versions = [
     "11", "12", "13", "14"
     ]
 
+subprocess.check_call("docker pull ubuntu:xenial", shell=True)
+subprocess.check_call("docker pull ubuntu:bionic", shell=True)
+subprocess.check_call("docker pull ubuntu:focal", shell=True)
+
+test_versions = {"4": "4.0", "5": "5.0", "6": "6.0"}
+
 if len(sys.argv) == 2:
     repo = sys.argv[1]
 
-for version in versions:
-    cmd = f"docker build clang-{version} -t {repo}:{version}"
+
+def build(version):
+    cmd = f"docker build --no-cache --tag {repo}:{version} clang-{version}"
     print(cmd)
     try:
         subprocess.check_call(cmd, shell=True)
     except:
         print("Failure in command: " + cmd)
         raise
+
+
+def test(version):
+    tv = version
+    if version in test_versions:
+        tv = test_versions[version]
+
+    cmd = f"docker run --rm {repo}:{version} clang++-{tv} --version"
+    expected = f"clang version {tv}"
+    print(cmd)
+    try:
+        output = subprocess.check_output(cmd, shell=True)
+        if not expected in output.decode():
+            msg += f"Expected output: \n{expected}\n"
+            msg += f"Not found in actual output: \n{output}\n"
+            raise AssertionError(msg)
+    except:
+        print("Failure in command: " + cmd)
+        raise
+
+
+def tag(version):
+    timestamp = datetime.datetime.utcnow().strftime("%Y%m%d_%H%M")
+    cmd = f"docker tag {repo}:{version} {repo}:{version}_{timestamp}"
+    try:
+        subprocess.check_call(cmd, shell=True)
+    except:
+        print("Failure in command: " + cmd)
+        raise
+
+
+for version in versions:
+    build(version)
+    test(version)
+    tag(version)
